6b0caa5500080694f25094f8d2a3e342bb45e6f4
Christian Wenz <christian@wenz.org>
2022-05-13 12:04:46 +0200

    03_04

--- BlazorWasmApp1/Client/Program.cs
+++ BlazorWasmApp1/Client/Program.cs
@@ -1,6 +1,6 @@
-using BlazorWasmApp1.Client;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using BlazorWasmApp1.Client;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
