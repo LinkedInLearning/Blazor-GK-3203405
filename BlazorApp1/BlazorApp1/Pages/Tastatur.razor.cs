e71faeb43996dda531d3e7223dafc677f72262a4
Christian Wenz <christian@wenz.org>
2022-05-13 12:02:17 +0200

    02_06

---
+++ BlazorApp1/BlazorApp1/Pages/Tastatur.razor.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Components.Web;
+
+namespace BlazorApp1.Pages
+{
+    public partial class Tastatur
+    {
+        private string taste = string.Empty;
+        private string wert = string.Empty;
+
+        private void VerarbeiteTaste(KeyboardEventArgs e)
+        {
+            taste = e.Key;
+        }
+    }
+}
